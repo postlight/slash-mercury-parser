4d06baeac23a4d832b3796967c2d61d931a3aef9
Jad Termsani <jadtermsani@gmail.com>
2019-03-08 16:13:52 +0200

    feat: split functions

--- src/parse.ts
+++ src/parse.ts
@@ -1,29 +1,25 @@
-import qs from 'qs';
-import request from 'request';
+import AWS from 'aws-sdk';
 
-import { APIGatewayProxyEvent } from 'aws-lambda';
-import { runWarm, getSlackData, validateUrl, getUrlContent } from './utils';
+const lambda = new AWS.Lambda();
 
-const parse = async (event: APIGatewayProxyEvent) => {
-  const req = qs.parse(event.body || '');
-  const url = req.text;
-
-  try {
-    await validateUrl(url);
-    const content = await getUrlContent(url);
-    const slackData = await getSlackData(req, content);
-    request.post('https://slack.com/api/files.upload', slackData);
-
-    return {
-      statusCode: 200,
-      body: 'Getting your article',
-    };
-  } catch (err) {
-    return {
-      statusCode: 200,
-      body: err.message,
-    };
-  }
-};
-
-export default runWarm(parse);
+export default async function invokeLambda({ body }: any) {
+  console.log('invoking second lambda function');
+  lambda.invoke(
+    {
+      FunctionName: 'serverless-mercury-slackbot-production-uploadPost',
+      Payload: JSON.stringify(body),
+      InvocationType: 'Event',
+    },
+    (err, data) => {
+      if (err) {
+        console.log('INVOKE ERROR', err);
+        return;
+      }
+      console.log('Success', data);
+      return {
+        statusCode: 200,
+        body: 'parsing your article',
+      };
+    }
+  );
+}
